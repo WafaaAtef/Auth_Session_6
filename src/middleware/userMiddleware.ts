ddb600d949c7284d8666852cac857dcb2639d539
yasmienalaa <yasmirnalaa167@gmail.com>
2026-08-16 00:10:41 +0300

    lastEdit

--- src/middleware/userMiddleware.ts
+++ src/middleware/userMiddleware.ts
@@ -5,12 +5,12 @@ import { nextTick } from "node:process";
 const auth = (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies?.token;
     if (!token) {
-        return res.status(401).json({ msg: "Unauthenticated" })
+        return res.status(401).json({ msg: "No token provided" })
     }
     try {
         const verify = jwt.verify(token, process.env.JWT_SECRET as string)
         if (!verify) {
-            return res.status(401).json({ msg: "Unauthenticated" })
+            return res.status(401).json({ msg: "Invalid token" })
         }
         next();
     } catch {
@@ -21,16 +21,19 @@ const auth = (req: Request, res: Response, next: NextFunction) => {
 const authorize = (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies?.token;
     if (!token) {
-        return res.status(401).json({ msg: "Unauthenticated" })
+        return res.status(401).json({ msg: "No token provided" })
     }
     try {
-        const verify = jwt.verify(token, process.env.JWT_SECRET as string) as { role: string }
+        const verify = jwt.verify(
+            token, process.env.JWT_SECRET as string
+        ) as { role: string }
         if (verify.role !== "admin") {
-            return res.status(403).json({ msg: "Unauthorized" })
+            return res.status(403).json({ msg: "Access denied" })
         }
         next();
     } catch {
         return res.status(401).json({ msg: "Invalid token" })
     }
 }
+
 export {auth, authorize}
